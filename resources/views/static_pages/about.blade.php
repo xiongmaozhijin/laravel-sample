eeb198b7a81eb06cdc5c378534b68e586ec949c0
baoxing <312904694@qq.com>
2018-10-30 15:22:40 +0800

    finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+
+@section('title', '关于2')
+
+@section('content')
+  <h1>关于页1</h1>
+@stop
+
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '帮助2')
+
+@section('content')
+  <h1>帮助页1</h1>
+@stop
