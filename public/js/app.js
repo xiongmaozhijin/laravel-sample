0e528dc4cf858aa5292729f58aed254811f0210a
baoxing <312904694@qq.com>
2018-11-01 07:46:03 +0000

    finish user edit, update, index, and destroy action

--- public/js/app.js
+++ public/js/app.js
@@ -43302,7 +43302,7 @@ if (false) {
 /* 42 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n/* Users index */\n                ^\n      Invalid CSS after \"/* Users index */\": expected 1 selector or at-rule, was \".\"\n      in /home/vagrant/code/sample/resources/assets/sass/app.scss (line 218, column 18)\n    at runLoaders (/home/vagrant/code/sample/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/vagrant/code/sample/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/vagrant/code/sample/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/vagrant/code/sample/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/vagrant/code/sample/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/home/vagrant/code/sample/node_modules/neo-async/async.js:8077:18)\n    at options.error (/home/vagrant/code/sample/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 /******/ ]);
